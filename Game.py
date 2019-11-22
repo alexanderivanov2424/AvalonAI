2051d640211d791132bfb3af86abc3ddfd44f009
alexanderivanov2424 <alexander_ivanov@brown.edu>
2019-11-22 17:57:27 -0500

    added game stencil

---
+++ Game.py
@@ -0,0 +1,34 @@
+
+
+class Avalon:
+
+    def __init__():
+        #5 player version
+        # M G G E E
+        pass
+
+    def game_start():
+        #give first input to each agent
+        #first input is just info about player roles
+        #all zeros unless own role is merlin
+        pass
+
+    def show_state():
+        #everyone sees current state of all quests
+        pass
+
+    def game_pick_team():
+        #have leader select team
+        pass
+
+    def show_team():
+        #everyone sees selected team and who picked it
+        pass
+
+    def game_team_vote():
+        #members of quest vote
+        pass
+
+    def show_quest():
+        #everyone sees quest result
+        pass

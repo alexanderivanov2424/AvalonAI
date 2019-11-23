15a5d9daa36c5946d19b10951fcb52e955d8a11c
alexanderivanov2424 <alexander_ivanov@brown.edu>
2019-11-23 10:28:56 -0500

    added player

--- Game.py
+++ Game.py
@@ -1,34 +1,48 @@
 
 #http://upload.snakesandlattes.com/rules/r/ResistanceAvalon.pdf
 class Avalon:
+    """
+    Roles:  -1  0  1  2
+             E  ?  G  M
 
-    def __init__():
+    Sides:  -1  0  1
+             E  ?  G
+
+    Quest:  -1  0  1
+             F  ?  P
+    """
+
+    def __init__(self, players):
         #5 player version
         # M G G E E
+        assert(len(players) = 5)
+        self.roles = np.random.shuffle(np.array([-1,-1,1,1,2]))
+        self.leader = np.random.randint(5)
+        self.quests = np.zeros()
         pass
 
-    def game_start():
+    def start_game(self):
         #give first input to each agent
         #first input is just info about player roles
         #all zeros unless own role is merlin
         pass
 
-    def show_state():
-        #everyone sees current state of all quests
-        pass
-
-    def game_pick_team():
+    def get_team(self):
         #have leader select team
         pass
 
-    def show_team():
+    def vote_team(self):
+        #everyone sees proposed team and votes
+        pass
+
+    def show_team(self):
         #everyone sees selected team and who picked it
         pass
 
-    def game_team_vote():
+    def vote_quest(self):
         #members of quest vote
         pass
 
-    def show_quest():
+    def show_quest(self):
         #everyone sees quest result
         pass
---
+++ Player.py
@@ -0,0 +1,27 @@
+
+
+class Player:
+
+    def __init__():
+        pass
+
+    def see_start():
+        pass
+
+    def pick_team():
+        pass
+
+    def vote_team():
+        pass
+
+    def show_team():
+        pass
+
+    def vote_quest():
+        pass
+
+    def see_quest():
+        pass
+
+    def guess_merlin():
+        pass

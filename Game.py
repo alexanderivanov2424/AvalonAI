5f8e3faf9f9d666caeb16b01034d9637d8cf1c4d
alexanderivanov2424 <alexander_ivanov@brown.edu>
2019-11-23 13:03:44 -0500

    added team vote to state and to mask

--- Game.py
+++ Game.py
@@ -25,8 +25,11 @@ class Avalon:
     ###########################################
     own side            (1)             -1   1
     own role            (1)             -1   1 2
-    is leader           (1)                0 1
 
+    is leader           (1)                0 1
+    is on quest         (1)                0 1
+
+    need team prop      (1)                0 1
     need team vote      (1)                0 1
     need quest vote     (1)                0 1
     quest result        (1)                0 1
@@ -39,6 +42,7 @@ class Avalon:
 
     team proposition    (5)                0 1
     team selected       (5)                0 1
+    team votes          (5)                0 1
     """
 
     def __init__(self, players):
@@ -52,59 +56,123 @@ class Avalon:
         self.sides = (self.roles > 0) * 2 - 1
         self.leader = np.random.randint(self.N)
         self.quests = np.zeros(self.N)
+
+        self.team = np.zeros(self.N)
+        self.team_vote = np.zeros(self.N)
+
+        self.state_size = 43
         pass
 
-    def get_visible_roles(self,role):
+    def get_visible_roles(self,role, i):
         assert(role in [-1,1,2])
         if role == -1:
             return self.roles * (self.roles == -1)
         if role == 1:
-            return self.roles * 0
-        if role = 2:
+            mask = np.zeros(self.N)
+            mask[i] = 1
+            return self.roles * mask
+        if role == 2:
             return self.roles
 
-    def get_visible_sides(self,role):
+    def get_visible_sides(self,role, i):
         assert(role in [-1,1,2])
-        if role in [-1,2]:
+        if role == -1:
             return self.sides
         if role == 1:
-            return self.sides * 0
+            mask = np.zeros(self.N)
+            mask[i] = 1
+            return self.sides * mask
+        if role == 2:
+            return self.sides
 
     def get_visible_leader(self):
         leaders = np.zeros(self.N)
         leaders[self.leader] = 1
         return leaders
 
-    def get_state(self, i, quest_r, team_p = np.zeros(self.N), team_s = np.zeros(self.N)):
-        state = np.zeros(36)
+    def get_state(self, i, on_quest = 0, quest_r = 0, team_p = None, team_s = None, team_v = None):
+        if team_p == None:
+            team_p = np.zeros(self.N)
+        if team_s == None:
+            team_s = np.zeros(self.N)
+        if team_v == None:
+            team_v = np.zeros(self.N)
+
+        state = np.zeros(self.state_size)
+
         state[0] = self.sides[i]
         state[1] = self.roles[i]
+
         state[2] = 1 if i == self.leader else 0
+        state[3] = on_quest
 
-        state[3] = 1
-        state[4] = 1
-        state[5] = quest_r
+        state[4] = 1 #need team proposition
+        state[5] = 1 #need team vote
+        state[6] = 1 #need quest vote
+        state[7] = quest_r
 
-        state[6:11] = self.get_visible_sides(state[0])
-        state[11:16] = self.get_visible_roles(state[1])
-        state[16:21] = self.get_visible_leader()
+        state[8:13] = self.get_visible_sides(state[1], i)
+        state[13:18] = self.get_visible_roles(state[1], i)
+        state[18:23] = self.get_visible_leader()
 
-        state[21:26] = self.quests
+        state[23:28] = self.quests
 
-        state[26:31] = team_p
-        state[31:36] = team_s
+        state[28:33] = team_p #proposed team
+        state[33:38] = team_s #selected team
+        state[38:43] = team_v #who voted how
+
         return state
+
+    def mask_state(self, state, mode = 'all'):
+        mask = np.zeros(self.state_size)
+        mask[0] = 1 #always show own side
+        mask[1] = 1 #always show own role
+        mask[2] = 1 #always show leader
+        mask[23:28] = 1 #always show current quests
+
+        if mode == 'all':
+            mask[:] = 1
+        if mode == 'start':
+            mask[8:23] = 1 #show visible sides, roles, leaders
+        if mode == 'team_prop':
+            mask[4] = 1 #nead team prop
+            mask[18:23] = 1 #show leaders
+        if mode == 'team_vote':
+            mask[3] = 1 #need team vote
+            mask[18:23] = 1 #show leaders
+            mask[23:28] = 1 #show proposition
+        if mode == 'team_fail_info':
+            mask[18:23] = 1 #show leaders
+            mask[38:43] = 1 #show votes
+        if mode == 'team_pass_info':
+            mask[18:23] = 1 #show leaders
+            mask[23:28] = 1 #show team
+            mask[38:43] = 1 #show votes
+        if mode == 'quest_vote':
+            mask[6] = 1 #need quest vote
+            mask[23:28] = 1 #show team
+        if mode == 'quest_info':
+            mask[7] = 1 #quest result
+            mask[18:23] = 1 #show leaders
+            mask[23:28] = 1 #show team
+        if mode == 'none':
+            pass
+        return state * mask
 
     def start_game(self):
         #give first input to each agent
         for i,player in enumerate(self.players):
-            state = self.get_state(i,0)
-            print(state)
-        pass
+            state = self.get_state(i)
+            state = self.mask_state(state,'start')
+            player.see_start(state)
 
     def get_team(self):
         #have leader select team
-        pass
+        player = self.players[self.leader]
+        state = self.get_state(self.leader)
+        state = self.mask_state(state,'team_prop')
+        team = player.pick_team(state)
+
 
     def vote_team(self):
         #everyone sees proposed team and votes
--- Player.py
+++ Player.py
@@ -6,22 +6,36 @@ class Player:
         pass
 
     def see_start(self):
+        #show player start state
+        #no return
         pass
 
     def pick_team(self):
+        #show player state
+        #request for team to be selected
         pass
 
     def vote_team(self):
+        #inform player of proposed team
+        #request for team vote
         pass
 
     def show_team(self):
+        #inform player of selected team (or if no team picked)
+        #show player who votes
+        #no return
         pass
 
     def vote_quest(self):
+        #show who is on team
+        #request for quest vote
         pass
 
     def see_quest(self):
+        #show quest result
+        #no return
         pass
 
     def guess_merlin(self):
+        #request guess for merlin
         pass

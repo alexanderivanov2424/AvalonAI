9880737bff37e866c9c1e0313e18a02b1fbc5a7e
alexanderivanov2424 <alexander_ivanov@brown.edu>
2019-11-23 11:44:04 -0500

    setting state method

--- Game.py
+++ Game.py
@@ -1,7 +1,15 @@
+import numpy as np
+
+from Player import Player
+
 
 #http://upload.snakesandlattes.com/rules/r/ResistanceAvalon.pdf
 class Avalon:
     """
+    5 player version
+    M G G E E
+
+
     Roles:  -1  0  1  2
              E  ?  G  M
 
@@ -10,21 +18,88 @@ class Avalon:
 
     Quest:  -1  0  1
              F  ?  P
+
+
+    State:
+    contents            size            values
+    ###########################################
+    own side            (1)             -1   1
+    own role            (1)             -1   1 2
+    is leader           (1)                0 1
+
+    need team vote      (1)                0 1
+    need quest vote     (1)                0 1
+    quest result        (1)                0 1
+
+    visible sides       (5)             -1 0 1
+    visible roles       (5)             -1 0 1 2
+    who is leader       (5)                0 1
+
+    current quests      (5)             -1 0 1
+
+    team proposition    (5)                0 1
+    team selected       (5)                0 1
     """
 
     def __init__(self, players):
-        #5 player version
-        # M G G E E
-        assert(len(players) = 5)
-        self.roles = np.random.shuffle(np.array([-1,-1,1,1,2]))
-        self.leader = np.random.randint(5)
-        self.quests = np.zeros()
+        assert(len(players) == 5)
+
+        self.N = 5
+        self.players = players
+        self.roles = np.array([-1,-1,1,1,2])
+
+        np.random.shuffle(self.roles)
+        self.sides = (self.roles > 0) * 2 - 1
+        self.leader = np.random.randint(self.N)
+        self.quests = np.zeros(self.N)
         pass
+
+    def get_visible_roles(self,role):
+        assert(role in [-1,1,2])
+        if role == -1:
+            return self.roles * (self.roles == -1)
+        if role == 1:
+            return self.roles * 0
+        if role = 2:
+            return self.roles
+
+    def get_visible_sides(self,role):
+        assert(role in [-1,1,2])
+        if role in [-1,2]:
+            return self.sides
+        if role == 1:
+            return self.sides * 0
+
+    def get_visible_leader(self):
+        leaders = np.zeros(self.N)
+        leaders[self.leader] = 1
+        return leaders
+
+    def get_state(self, i, quest_r, team_p = np.zeros(self.N), team_s = np.zeros(self.N)):
+        state = np.zeros(36)
+        state[0] = self.sides[i]
+        state[1] = self.roles[i]
+        state[2] = 1 if i == self.leader else 0
+
+        state[3] = 1
+        state[4] = 1
+        state[5] = quest_r
+
+        state[6:11] = self.get_visible_sides(state[0])
+        state[11:16] = self.get_visible_roles(state[1])
+        state[16:21] = self.get_visible_leader()
+
+        state[21:26] = self.quests
+
+        state[26:31] = team_p
+        state[31:36] = team_s
+        return state
 
     def start_game(self):
         #give first input to each agent
-        #first input is just info about player roles
-        #all zeros unless own role is merlin
+        for i,player in enumerate(self.players):
+            state = self.get_state(i,0)
+            print(state)
         pass
 
     def get_team(self):
@@ -46,3 +121,10 @@ class Avalon:
     def show_quest(self):
         #everyone sees quest result
         pass
+
+
+
+#Testing
+players = [Player() for i in range(5)]
+game = Avalon(players)
+game.start_game()
--- Player.py
+++ Player.py
@@ -2,26 +2,26 @@
 
 class Player:
 
-    def __init__():
+    def __init__(self):
         pass
 
-    def see_start():
+    def see_start(self):
         pass
 
-    def pick_team():
+    def pick_team(self):
         pass
 
-    def vote_team():
+    def vote_team(self):
         pass
 
-    def show_team():
+    def show_team(self):
         pass
 
-    def vote_quest():
+    def vote_quest(self):
         pass
 
-    def see_quest():
+    def see_quest(self):
         pass
 
-    def guess_merlin():
+    def guess_merlin(self):
         pass

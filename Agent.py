fixed gradients issue and leader incrementing bug

Agent.py
@@ -43,10 +43,16 @@ class AvalonPlayer(Player):
         self.side_guess_list = []
         self.action_logit_list = []
 
+        self.proposed_team_mask = tf.convert_to_tensor([1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],dtype='float32')
+        self.team_vote_mask =     tf.convert_to_tensor([0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],dtype='float32')
+        self.quest_vote_mask =    tf.convert_to_tensor([0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],dtype='float32')
+        self.merlin_guess_mask =  tf.convert_to_tensor([0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0],dtype='float32')
+        self.side_guess_mask =    tf.convert_to_tensor([0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],dtype='float32')
+
     def run_model(self, state):
         actions, self.hidden = self.model.call(state, self.hidden)
-        self.merlin_guess_list.append(tf.slice(actions, [7], [5]))
-        self.side_guess_list.append(tf.slice(actions, [12], [5]))
+        self.merlin_guess_list.append(actions * self.merlin_guess_mask)
+        self.side_guess_list.append(actions * self.side_guess_mask)
         return actions
 
     def reset(self):
@@ -55,11 +61,12 @@ class AvalonPlayer(Player):
         self.action_logit_list = []
 
     def loss_function(self, true_sides, true_merlin, did_win):
+        true_merlin = tf.concat([tf.zeros(7),true_merlin,tf.zeros(5)], 0)
+        true_sides = tf.concat([tf.zeros(12),true_sides], 0)
         loss = 0
         reward = 10 if did_win else 1
         for action_logit in reversed(self.action_logit_list):
             diff = action_logit - 0.5
-            print(tf.gradients(action_logit))
             loss += -diff * diff * reward
             reward *= 0.99
         for guess in self.merlin_guess_list:
@@ -77,15 +84,14 @@ class AvalonPlayer(Player):
         # show player state
         # request for team to be selected
         actions = self.run_model(state)
-        self.action_logit_list.append(tf.slice(actions, [0], [5]))
+        self.action_logit_list.append(actions * self.proposed_team_mask)
         return actions[0:5]
 
     def vote_team(self, state):
         # inform player of proposed team
         # request for team vote
         actions = self.run_model(state)
-        self.action_logit_list.append(tf.slice(actions, [5], [1]))
-        print(actions[5])
+        self.action_logit_list.append(actions * self.team_vote_mask)
         return actions[5] > 0.5
 
     def show_team(self, state):
@@ -99,7 +105,7 @@ class AvalonPlayer(Player):
         # show who is on team
         # request for quest vote
         actions = self.run_model(state)
-        self.action_logit_list.append(tf.slice(actions, [6], [1]))
+        self.action_logit_list.append(actions * self.quest_vote_mask)
         return actions[6] > 0.5
 
     def see_quest(self, state):

AvalonTrainer.py
@@ -1,40 +1,55 @@
 from Agent import AvalonPlayer
 from Game import Avalon
 import tensorflow as tf
+import numpy as np
 
-players = [AvalonPlayer() for i in range(5)]
-game = Avalon(players)
+def train(players):
+    player_losses = [None for p in players]
 
+    with tf.GradientTape(persistent=True) as tape:
+        game = Avalon(players)
+        game.start_game()
+        while not game.is_over():
+            game.get_team()
+            game.vote_team()
+            game.show_team()
+            if game.team_r:  # extracting vote result
+                game.vote_quest()
+                game.show_quest()
 
-def train():
-    game.start_game()
-    while not game.is_over():
-        print("get team")
-        game.get_team()
-        print("vote team")
-        game.vote_team()
-        print("show team")
-        game.show_team()
-        if game.team_r:  # extracting vote result
-            print("vote team")
-            game.vote_quest()
-            print("show team")
-            game.show_quest()
+        if game.winning_side() == 1:
+            game.guess_merlin()
 
-    if game.winning_side() == 1:
-        game.guess_merlin()
+        result = game.get_game_result()
+        true_merlin = tf.constant(game.roles == 2, dtype="float32")
+        true_sides = tf.constant(game.sides == 1, dtype="float32")
 
-    result = game.get_game_result()
-    true_merlin = tf.constant(game.roles == 2, dtype="float32")
-    true_sides = tf.constant(game.sides == 1, dtype="float32")
+        for i, player in enumerate(players):
+            player_losses[i] = player.loss_function(true_sides, true_merlin, result[i])
 
     for i, player in enumerate(players):
-        with tf.GradientTape() as tape:
-            loss = player.loss_function(true_merlin, true_sides, result[i])
+        loss = player_losses[i]
         gradients = tape.gradient(loss, player.model.trainable_variables)
-        player.model.optimizer.apply_gradients(
-            zip(gradients, player.model.trainable_variables)
-        )
+        player.model.optimizer.apply_gradients(zip(gradients, player.model.trainable_variables))
+
+    return np.mean(player_losses)
 
 
-train()
+
+path = '/gpfs/main/home/aivanov6/course/cs1470/Final/AvalonAI/saved_model/AvalonAI_'
+version = 1
+path = path + str(version)
+
+players = [AvalonPlayer() for i in range(5)]
+
+for player in players:
+    try:
+        player.model.load_weights(path)
+    except:
+        pass
+
+for i in range(100):
+    loss = train(players)
+    print("LOSS: ", loss)
+
+players[0].model.save_weights(path)